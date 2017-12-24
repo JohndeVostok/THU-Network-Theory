b9606f9b78c703710c1288efaec64b0319f4ccce
mazx <dyxdy@live.com>
2017-12-24 18:46:35 +0800

    Add qthread with bug.

--- server/main.cpp
+++ server/main.cpp
@@ -5,11 +5,32 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <pthread.h>
+
+pthread_t threads[128];
+int threads_size = 0;
+
+void recv_data(int client_sockfd)
+{
+	char buf[BUFSIZ];
+	memset(buf, 0, BUFSIZ);
+	int len;
+	while ((len = recv(client_sockfd, buf, BUFSIZ, 0)) > 0)
+	{
+		buf[len] = '\0';
+		printf("%s\n", buf);
+		if (send(client_sockfd, buf, len, 0) < 0)
+		{
+			perror("write error");
+			return;
+		}
+	}
+	close(client_sockfd);
+}
 
 int main(int argc, char *argv[])
 {
 	int server_sockfd;
-	int client_sockfd;
 	int len;
 	struct sockaddr_in my_addr;
 	struct sockaddr_in remote_addr;
@@ -32,34 +53,26 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	if (listen(server_sockfd, 20) < 0)
+	if (listen(server_sockfd, 64) < 0)
 	{
 		perror("listen error");
 		return 1;
 	}
 
+	memset(threads, 0, sizeof(threads));
+	
 	sin_size = sizeof(struct sockaddr_in);
-	if ((client_sockfd = accept(server_sockfd, (struct sockaddr *)&remote_addr, &sin_size)) < 0)
+	while(1)
 	{
-		perror("accept error");
-		return 1;
-	}
-
-	printf("accept client %s/n", inet_ntoa(remote_addr.sin_addr));
-	len = send(client_sockfd, "Welcome", 21, 0);
-
-	while ((len = recv(client_sockfd, buf, BUFSIZ, 0)) > 0)
-	{
-		buf[len] = '\0';
-		printf("%s\n", buf);
-		if (send(client_sockfd, buf, len, 0) < 0)
+		int client_sockfd;
+		if ((client_sockfd = accept(server_sockfd, (struct sockaddr *)&remote_addr, &sin_size)) < 0)
 		{
-			perror("write error");
+			perror("accept error");
 			return 1;
 		}
+		thread *client = new thread(recv_data, client_sockfd);
 	}
-
-	close(client_sockfd);
 	close(server_sockfd);
+	return 0;
 }
 
